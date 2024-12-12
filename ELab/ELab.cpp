903cd47c1fbd1cb629e2eaa6583d33396d8e8199
sssllliiihhh <lihobabinslavik93@gmail.com>
2024-12-12 13:09:32 +0300

    12.12.2024

--- ELab/ELab.cpp
+++ ELab/ELab.cpp
@@ -1,4 +1,5 @@
 ﻿#include <SDL.h>
+#include <iostream>
 
 SDL_Event ev;
 
@@ -6,24 +7,39 @@ int main(int argc, char* argv[])
 {
     bool run = true;
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+    SDL_DisplayMode desktopMode;
+    SDL_GetDesktopDisplayMode(0, &desktopMode);
+
+    //std::string s = desktopMode.w + "|" + desktopMode.h;
+    SDL_Window* window = SDL_CreateWindow("ELab", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+
+    std::cout << desktopMode.w << "|" << desktopMode.h << std::endl;
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 250, 250, 250, 255);
     SDL_RenderClear(renderer);
 
     SDL_RenderPresent(renderer); SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
-
     while (run)
     {
-        while (SDL_PollEvent(&ev))
+        SDL_PollEvent(&ev);
+        switch (ev.type)
         {
-            SDL_Delay(16);
-            if (ev.type == SDL_QUIT)
-            {
+            case SDL_QUIT:
                 run = false;
-            }
+                break;
+            case SDL_WINDOWEVENT:
+                if (ev.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                    SDL_SetRenderDrawColor(renderer, 250, 250, 250, 255);
+                    SDL_RenderClear(renderer);
+
+                    SDL_RenderPresent(renderer); SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+                }
+                break;
         }
+        SDL_Delay(16);
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
